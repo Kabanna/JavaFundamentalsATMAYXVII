fcfd4ce1503e4854d2e8a19c3dd174525c8735e9
anna shevelieva <shevelann@rambler.ru>
2017-08-02 21:34:00 +0300

    homework 1

---
+++ src/ua/org/oa/anna/shevelieva/home_work_ATMAYXVII_3/Patient.java
@@ -0,0 +1,47 @@
+/**
+ * Create a java program that satisfies the following requirements:
+ * 1. Create a class that describes the given object. In each class:
+ * Define a minimum of 5 fields of three different types characterizing the given object;
+ * Create a method that calculates parameters based on the values ​​of the object fields;
+ * Define a method for displaying information about an object on the screen.
+ * 2. Each class, field, method should have a name that reflects its essence and comments describing their purpose.
+ * 3. At the stage of programming, agreements from the java code convention should be used.
+ * 4. Test the class you created by creating several instances of the class (in the main () method) that describes the object, initializing the attribute variables of each instance, and calling its methods.
+ * 5. The name of the package in which the class / class is created must be in the format ua.org.oa. <Jira Login>.
+ * 6. The indivudual folder in the SVN repository to save the completed tasks is located at http://oracle-academy.org.ua/svn/<Project Name> / <Jira login>
+
+ * Object:
+ * Patient of the hospital
+ *
+ */
+
+package ua.org.oa.anna.shevelieva.home_work_ATMAYXVII_3;
+
+public class Patient {
+    private String patientName;
+    private int patientAge;
+    private String  patientGender;
+    private String disease;
+    private Double height;
+
+    Patient (String patientName, int patientAge, String patientGender, String disease, Double height)
+    {
+        this.patientName=patientName;
+        this.patientGender=patientGender;
+        this.patientAge=patientAge;
+        this.disease=disease;
+        this.height=height;
+    }
+
+    /**
+     * Print list of Patients
+     */
+    public void printPatientInformation(){
+        System.out.format(" Patient: %s\n Gender: %s\n " + "Age: %d\n Height: %f\n" + " Disease: %s\n" ,
+                this.patientName,
+                this.patientGender,
+                this.patientAge,
+                this.height,
+                this.disease);
+    }
+}
---
+++ src/ua/org/oa/anna/shevelieva/home_work_ATMAYXVII_3/PatientTest.java
@@ -0,0 +1,27 @@
+package ua.org.oa.anna.shevelieva.home_work_ATMAYXVII_3;
+
+public class PatientTest {
+    public static void main(String[] args) {
+
+        Patient p1 = new Patient ("Misha ",12,"Male","heart patient",190.9);
+        Patient p2 = new Patient ("Sara ",23,"Female","earnose",185.4);
+        Patient p3 = new Patient ("Alla ",31,"Female","earnose",180.0);
+        Patient p4 = new Patient ("Eugene ",15,"Male","heart patient",174.0);
+        Patient p5 = new Patient ("Roma ",18,"Male","eye",169.0);
+        Patient p6 = new Patient ("Sem ",21,"Male","eye", 163.0);
+
+        System.out.println("~~~ Information about patient ~~~");
+        p1.printPatientInformation();
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        p2.printPatientInformation();
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        p3.printPatientInformation();
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        p4.printPatientInformation();
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        p5.printPatientInformation();
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        p6.printPatientInformation();
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    }
+}
